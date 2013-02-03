be7453fc8821fe58bff30f90f2183fb5ec284681
root <root@li568-8.members.linode.com>
2013-02-03 06:40:34 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TongRending::Application.config.secret_token = '26fcc2148b2146909e47544842192eb8379d98018232fa9de714ab2aedbb1a0cfbb2382a814847481a7bf70b37ea19430b0258b4849e31d7ec6d63f890aae848'
